f01716b6e68cda27e85c9bbe3a1652e94327a24f
renzo <renzobocanegra.a@gmail.com>
2024-04-23 15:18:31 -0500

    feat: add header

---
+++ frontend/components/Header.tsx
@@ -0,0 +1,44 @@
+import qs from "qs";
+import Image from "next/image";
+import axios from "axios";
+import { flattenAttributes } from "@/lib/utils";
+import Link from "next/link";
+import { CategoryLinkType } from "@/types/category";
+import Navbar from "./Navbar";
+
+const BASE_URL = process.env.BASE_URL!;
+// const currentDate = new Date();
+
+const query = qs.stringify({
+  fields: ["name", "slug"],
+});
+
+const getCategoriesData = async () => {
+  const { data } = await axios(`${BASE_URL}/api/categories?${query}`);
+  return flattenAttributes(data.data) as CategoryLinkType[];
+};
+
+const Header = async () => {
+  const categories = await getCategoriesData();
+
+  return (
+    <header>
+      <div className="flex w-full flex-row items-center justify-start border-b bg-gradient-to-r from-custom-blue to-custom-green p-4 md:justify-center">
+        <Link href="/" prefetch={false}>
+          <Image
+            src="/assets/logo-extended-white.svg"
+            alt="noticias-360-logo"
+            width={330}
+            height={100}
+            className="w-28 md:w-40"
+          />
+        </Link>
+      </div>
+      <div className="flex w-full flex-row items-center justify-start p-4 text-xs shadow md:justify-center">
+        <Navbar categories={categories} />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ frontend/components/Navbar.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { CategoryLinkType } from "@/types/category";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React, { FC } from "react";
+
+interface NavbarProps {
+  categories: CategoryLinkType[];
+}
+
+const Navbar: FC<NavbarProps> = ({ categories }) => {
+  const pathname = usePathname();
+
+  return (
+    <nav className="overflow-x-auto">
+      <ul className="flex flex-nowrap gap-4 divide-x divide-custom-blue whitespace-nowrap font-semibold uppercase tracking-wider">
+        <li key="home">
+          <Link
+            href="/"
+            className={cn("hover:underline", { underline: pathname === "/" })}
+            prefetch={false}
+          >
+            Home
+          </Link>
+        </li>
+        {categories.map((category) => (
+          <li key={category.id}>
+            <Link
+              href={`/${category.slug}`}
+              className={cn("ml-4 hover:underline", {
+                underline: pathname === `/${category.slug}`,
+              })}
+              prefetch={false}
+            >
+              {category.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
