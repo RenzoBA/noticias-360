f73a1f7765009b3d30182f563da7c8e5e1deafb1
renzo <renzobocanegra.a@gmail.com>
2024-04-23 15:24:49 -0500

    refactor: code

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,13 +1,13 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   prefix: "",
   theme: {
     container: {
@@ -18,7 +18,16 @@ const config = {
       },
     },
     extend: {
+      aspectRatio: {
+        "3/2": "3 / 2",
+      },
       colors: {
+        custom: {
+          blue: "hsl(var(--custom-blue))",
+          "blue-light": "hsl(var(--custom-blue-light))",
+          purple: "hsl(var(--custom-purple))",
+          green: "hsl(var(--custom-green))",
+        },
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
@@ -75,6 +84,6 @@ const config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} satisfies Config;
 
-export default config
+export default config;
