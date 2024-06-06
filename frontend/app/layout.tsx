b3a703759f2878abc527048e0c1f7f3f9a4f0dae
renzo <renzobocanegra.a@gmail.com>
2024-06-06 18:58:28 -0500

    feat: add toaster

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -4,6 +4,7 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { Toaster } from "@/components/ui/sonner";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
@@ -34,6 +35,7 @@ export default function RootLayout({
         <Header />
         {children}
         <Footer />
+        <Toaster />
       </body>
     </html>
   );
